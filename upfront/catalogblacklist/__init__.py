47c909e8db6aa126aff995b3b04dac81c9e91781
Roché Compaan <roche@upfrontsystems.co.za>
2011-07-09 19:44:36 +0200

    remove patch
    init tool

--- upfront/catalogblacklist/__init__.py
+++ upfront/catalogblacklist/__init__.py
@@ -1,1 +1,7 @@
-import patch
+from Products.CMFPlone.utils import ToolInit
+from upfront.catalogblacklist.tool import CatalogBlacklist
+
+def initialize(context):
+    ToolInit('Plone Tool',
+             tools=(CatalogBlacklist,),
+             icon='tool.gif').initialize(context)
